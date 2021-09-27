cc1b4212f23e945255e216dd2dfc43247634c7ee
yhl1219 <yhliu2000@outlook.com>
2021-09-27 12:44:07 +0800

    [ch4] modify ch4_umap0.c to satisfy interface, fixed assert_eq

--- src/ch4_unmap0.c
+++ src/ch4_unmap0.c
@@ -14,10 +14,10 @@ int main()
 	int prot = 3;
 	assert_eq(0, mmap((void *)start, len, prot, MAP_ANONYMOUS, -1));
 	assert_eq(mmap((void *)(start + len), len * 2, prot, MAP_ANONYMOUS,
-		       -1), (len * 2));
-	assert_eq(munmap((void *)start, len), len);
+		       -1), 0);
+	assert_eq(munmap((void *)start, len), 0);
 	assert_eq(mmap((void *)(start - len), len + 1, prot, MAP_ANONYMOUS,
-		       -1), (len * 2));
+		       -1), 0);
 	for (uint64 i = (start - len); i < (start + len * 3); ++i) {
 		uint8 *addr = (uint8 *)i;
 		*addr = (uint8)i;
