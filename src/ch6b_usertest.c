2ec16592a1fb70ada3833b63e3c6de6ca09bf213
DeathWish5 <zyr_ms@outlook.com>
2021-10-05 18:20:55 +0800

    typo fix

--- src/ch6b_usertest.c
+++ src/ch6b_usertest.c
@@ -8,7 +8,7 @@
 const char *TESTS[] = {
 	"ch2b_hello_world\0", "ch2b_power\0",	  "ch2b_write1\0",
 	"ch3b_sleep\0",	      "ch3b_sleep1\0",	  "ch3b_yield0\0",
-	"ch3b_yield1\0",      "ch3b_yield2\0",	  "ch5b_gitpid\0",
+	"ch3b_yield1\0",      "ch3b_yield2\0",	  "ch5b_getpid\0",
 	"ch5b_forktest0\0",   "ch5b_forktest1\0", "ch5b_forktest2\0",
 	"ch6b_pipetest\0",
 };
