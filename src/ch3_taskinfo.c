3e9a993a5b591daf3f3456e181b210a805866910
rcy17 <rcycyzh@163.com>
2022-03-09 13:24:00 +0800

    Update ch3_taskinfo.c

--- src/ch3_taskinfo.c
+++ src/ch3_taskinfo.c
@@ -17,31 +17,33 @@ int main()
 	int64 t1 = get_mtime();
 	TaskInfo info;
 	get_mtime();
+	sleep(500);
 	int64 t2 = get_mtime();
 	// 注意本次 task info 调用也计入内
 	assert(sys_task_info(&info) == 0);
 	int64 t3 = get_mtime();
-	assert(3 == info.syscall_times[SYSCALL_GETTIMEOFDAY]);
+	// sleep 会用到获取时间的系统调用，所以是 <= 而不是 ==
+	assert(3 <= info.syscall_times[SYSCALL_GETTIMEOFDAY]);
 	assert(1 == info.syscall_times[SYSCALL_TASK_INFO]);
 	assert(0 == info.syscall_times[SYSCALL_WRITE]);
-	assert(0 == info.syscall_times[SYSCALL_YIELD]);
+	assert(0 < info.syscall_times[SYSCALL_YIELD]);
 	assert(0 == info.syscall_times[SYSCALL_EXIT]);
 	assert(t2 - t1 <= info.time);
-	assert(info.time < t3 - t1 + 10);
+	assert(info.time < t3 - t1 + 100);
 	assert(Running == info.status);
 
 	puts("hello task!");
 	int64 t4 = get_mtime();
-	// 想想为什么 write 调用是两次
+	// 看看puts的实现，想想为什么 write 调用只有一次
 	assert(sys_task_info(&info) == 0);
 	int64 t5 = get_mtime();
-	assert(5 == info.syscall_times[SYSCALL_GETTIMEOFDAY]);
+	assert(5 <= info.syscall_times[SYSCALL_GETTIMEOFDAY]);
 	assert(2 == info.syscall_times[SYSCALL_TASK_INFO]);
-	assert(2 == info.syscall_times[SYSCALL_WRITE]);
-	assert(0 == info.syscall_times[SYSCALL_YIELD]);
+	assert(1 == info.syscall_times[SYSCALL_WRITE]);
+	assert(0 < info.syscall_times[SYSCALL_YIELD]);
 	assert(0 == info.syscall_times[SYSCALL_EXIT]);
 	assert(t4 - t1 <= info.time);
-	assert(info.time < t5 - t1 + 10);
+	assert(info.time < t5 - t1 + 100);
 	assert(Running == info.status);
 	puts("Test task info OK!");
 	return 0;
