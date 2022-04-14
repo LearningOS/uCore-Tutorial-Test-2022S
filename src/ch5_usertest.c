7867f5ef32bd5656e7afb9251292e66517cf36ae
azd19 <azd19@mails.tsinghua.edu.cn>
2022-04-14 14:03:49 +0800

    add ch5 test

--- src/ch5_usertest.c
+++ src/ch5_usertest.c
@@ -5,17 +5,10 @@
 /// 辅助测例，运行所有其他测例。
 
 const char *TESTS[] = {
-	"ch2b_hello_world\0", "ch2b_power\0",	  "ch2b_write1\0",
-	"ch3b_sleep\0",	      "ch3b_sleep1\0",	  "ch3b_yield0\0",
-	"ch3b_yield1\0",      "ch3b_yield2\0",	  "ch5b_getpid\0",
-	"ch5b_forktest0\0",   "ch5b_forktest1\0", "ch5b_forktest2\0",
-	"ch4_mmap0\0",	      "ch4_mmap3\0",	  "ch4_unmap0\0",
-	"ch4_unmap1\0",	      "ch5_spawn0\0",	  "ch5_spawn1\0",
+	"ch5_spawn0\0",	  "ch5_spawn1\0",   "ch5_setprio\0",
 };
 
 const char *FAILS[] = {
-	"ch4_mmap1\0",
-	"ch4_mmap2\0",
 };
 
 int main()
