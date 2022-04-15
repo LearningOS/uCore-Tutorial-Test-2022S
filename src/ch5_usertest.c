368ee9d7c0089abd2073e45d2495560db6f41025
安之达 <azd19@mails.tsinghua.edu.cn>
2022-04-15 14:18:50 +0000

    Update ch5_usertest.c

--- src/ch5_usertest.c
+++ src/ch5_usertest.c
@@ -9,20 +9,25 @@ const char *TESTS[] = {
 	"ch3b_sleep\0",	      "ch3b_sleep1\0",	  "ch3b_yield0\0",
 	"ch3b_yield1\0",      "ch3b_yield2\0",	  "ch5b_getpid\0",
 	"ch5b_forktest0\0",   "ch5b_forktest1\0", "ch5b_forktest2\0",
-	"ch4_mmap0\0",	      "ch4_mmap3\0",	  "ch4_unmap0\0",
+	"ch4_mmap0\0",	      "ch4_mmap1\0",      "ch4_mmap2\0",
+    "ch4_mmap3\0",	      "ch4_unmap0\0",
 	"ch4_unmap1\0",	      "ch5_spawn0\0",	  "ch5_spawn1\0",
-	"ch5_setprio\0",	  "ch5t_usertest\0",
-};
-
-const char *FAILS[] = {
-	"ch4_mmap1\0",
-	"ch4_mmap2\0",
+	"ch5_setprio\0",
 };
 
 int main()
 {
-	int nsucc = sizeof(TESTS) / sizeof(char *);
-	int nfail = sizeof(FAILS) / sizeof(char *);
-	test(TESTS, nsucc, FAILS, nfail, "ch5 Usertests passed!");
+	int pid[20] = {0};
+	for (int i = 0; i < 20; i++) {
+		printf("Usertests: Running %s\n", TESTS[i]);
+		pid[i] = spawn(TESTS[i]);
+
+	}
+	int exit_pid = 0;
+	for(int i = 0; i < 20; i++) {
+        int wait_pid = waitpid(pid[i], &exit_pid);
+        assert_eq(wait_pid, pid[i]);
+    }
+    puts("ch5 usertest passed!");
 	return 0;
 }
