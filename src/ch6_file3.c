551a9c6722541c58d94d6aea5af08073df68e3ff
rcy17 <rcycyzh@163.com>
2022-05-01 15:46:13 +0800

    Add ch6_file3

---
+++ src/ch6_file3.c
@@ -0,0 +1,38 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/// 测试大量 open/unlink，输出 Test mass open/unlink OK! 就算正确。
+
+void repeat(char *target, const char *template, int times)
+{
+	int len = strlen(template);
+	for (int i = 0; i < times; i++)
+		stpncpy(target + len * i, template, len);
+}
+char test_str[4096];
+int main()
+{
+	char fname[] = "fname0";
+	puts("build");
+	repeat(test_str,
+	       "some random long long long long long long long long string",
+	       50);
+	int len = strlen(test_str);
+	puts("start iteration");
+	for (int i = 0; i < 10; i++) {
+		fname[5] = '0' + i; // change fname to "fname%d"
+		int fd = open(fname, O_CREATE | O_WRONLY);
+		assert(fd > 0);
+		for (int j = 0; j < 50; j++) {
+			write(fd, test_str, len);
+		}
+		close(fd);
+		assert_eq(unlink(fname), 0);
+		printf("test iteration %d\n", i);
+	}
+	puts("Test mass open/unlink OK!");
+	return 0;
+}
