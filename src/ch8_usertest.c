40dcb2cb7cfb2b8dc2750b24258dc6eb54f98bff
rcy17 <rcycyzh@163.com>
2022-05-20 20:52:07 +0800

    Adjust order of test

--- src/ch8_usertest.c
+++ src/ch8_usertest.c
@@ -10,10 +10,10 @@ const char *TESTS[] = {
 	"ch3b_sleep1\0",       "ch3b_yield0\0",	       "ch3b_yield1\0",
 	"ch3b_yield2\0",       "ch5b_getpid\0",	       "ch5b_forktest0\0",
 	"ch5b_forktest1\0",    "ch5b_forktest2\0",     "ch6b_filetest\0",
-	"ch6b_exec\0",	       "ch8b_mpsc_sem\0",      "ch8b_mut_race\0",
+	"ch6b_exec\0",	       "ch8_sem1_deadlock\0",  "ch8_sem2_deadlock\0",
+	"ch8_mut1_deadlock\0", "ch8b_mpsc_sem\0",      "ch8b_mut_race\0",
 	"ch8b_sync_sem\0",     "ch8b_test_condvar\0",  "ch8b_threads_arg\0",
-	"ch8b_threads\0",      "ch8b_spin_mut_race\0", "ch8_sem1_deadlock\0",
-	"ch8_sem2_deadlock\0", "ch8_mut1_deadlock\0",
+	"ch8b_threads\0",      "ch8b_spin_mut_race\0",
 };
 
 int main()
